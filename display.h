5f4ed83242c089a9af9f3f81f50ae06ab36de0eb
TheProgxy <theproxy@gmail.com>
2026-06-08 16:38:01 +0200

    add oled basic support missing glyphs

--- display.h
+++ display.h
@@ -66,4 +66,133 @@ class Display {
 	 }
 };
 
+#define DISPLAY_ADDRESS 0x3C
+
+typedef enum : byte {
+  COMMAND_BYTE        = 0x00,
+  PIXEL_BYTE          = 0x40,
+  DISPLAY_OFF         = 0xAE,
+  DISPLAY_ON          = 0xAF,
+  SET_ADDRESSING_MODE = 0x20,
+  SET_COLUMN_ADDRESS  = 0x21,
+  SET_CONTRAST        = 0x81,
+  SET_ENTIRE_DISPLAY_OFF     = 0xA4,
+  SET_MULTIPLEX       = 0xA8,
+  SET_DISPLAY_CLOCKDIV = 0xD5,
+  COMSCANDEC = 0xC8,
+  SET_SEGREMAP = 0xA0,
+  SET_COMPINS = 0xDA,
+  SET_STARTLINE = 0x40,
+  SET_DISPLAY_OFFSET = 0xD3,
+  SET_NORMAL_DISPLAY = 0xA6,
+  SET_CHARGE_PUMP_REGULATOR = 0x8D,
+  SET_PRECHARGE = 0xD9,
+  DEACTIVATE_SCROLL = 0x2E,
+  SET_VCOMDETECT = 0xDB,
+  SET_PAGE_ADDRESS = 0x22
+} DisplayCommands;
+
+#define DISPLAY_HEIGHT 64
+#define DISPLAY_WIDTH  128
+#define DISPLAY_COLS   128
+#define DISPLAY_PAGES  8
+
+#define MIN(a, b) ((a) > (b) ? (b) : (a))
+
+class OledDisplay {
+private:
+  byte display_buf[DISPLAY_COLS * DISPLAY_PAGES] = {0};
+  uint32_t display_idx = 0;
+
+	void write_pixel_buf(const byte* data, size_t count) {
+  		while (count) {
+			Wire.beginTransmission(DISPLAY_ADDRESS);
+	  		Wire.write((byte) PIXEL_BYTE);
+	  		const byte sent = Wire.write(data, MIN(count, BUFFER_LENGTH - 1));
+	  		count -= sent;
+   			data += sent;
+			byte status = Wire.endTransmission();
+  			if (status) Serial.println("Failed to send pixels.");
+  		}
+		return;
+	}
+
+	void send_command(const byte cmd, const byte* data, const size_t count) {
+		Wire.beginTransmission(DISPLAY_ADDRESS);
+  		Wire.write(COMMAND_BYTE);
+		Wire.write(cmd);
+  		if (count > 0 || data == NULL) Wire.write(data, count);
+		byte status = Wire.endTransmission();
+		if (status) Serial.println("Failed to send command.");
+		return;
+	}
+
+	void set_offset(byte page, byte col) {
+		byte cols[2] = {0x00, 0x7F};
+		cols[0] = col & 0x7F;
+		byte pages[2] = {0x00, 0x07};
+		pages[0] = page & 0x07;
+		this -> send_command(SET_PAGE_ADDRESS, pages, 2);
+		this -> send_command(SET_COLUMN_ADDRESS, cols, 2);
+		return;
+	}
+
+public:
+  void init(void) {
+ 	byte data = 63;
+ 	this -> send_command(DISPLAY_OFF, NULL, 0);
+    this -> send_command(SET_MULTIPLEX, &data, 1);
+ 	data = 0x00;
+    this -> send_command(SET_DISPLAY_OFFSET, &data, 1);
+ 	this -> send_command(SET_STARTLINE, NULL, 0);
+    this -> send_command(SET_SEGREMAP | 1, NULL, 0);
+    this -> send_command(COMSCANDEC, NULL, 0); //?
+    data = 0x12;
+    this -> send_command(SET_COMPINS, &data, 1);
+	data = 0x7F;
+    this -> send_command(SET_CONTRAST, &data, 1);
+    this -> send_command(SET_NORMAL_DISPLAY, NULL, 0);
+ 	data = 0x80;
+    this -> send_command(SET_DISPLAY_CLOCKDIV, &data, 1);
+ 	data = 0x14;
+    this -> send_command(SET_CHARGE_PUMP_REGULATOR, &data, 1);
+ 	data = 0xF1;
+ 	this -> send_command(SET_PRECHARGE, &data, 1);
+ 	data = 0x00;
+ 	this -> send_command(SET_ADDRESSING_MODE, &data, 1);
+    data = 0x40;
+ 	this -> send_command(SET_VCOMDETECT, &data, 1);
+ 	this -> send_command(SET_ENTIRE_DISPLAY_OFF, NULL, 0);
+    this -> send_command(DEACTIVATE_SCROLL, NULL, 0);
+	this -> send_command(DISPLAY_ON, NULL, 0);
+ 	this -> display();
+ 	return;
+  }
+
+  void print(const char c) {
+ 	const byte data[] = {0x00, 0x00, 0x1E, 0x05, 0x1E, 0x00, 0x00};
+ 	memcpy(this -> display_buf + this -> display_idx, data, sizeof(data));
+	this -> display_idx = (this -> display_idx + sizeof(data)) % sizeof(this -> display_buf);
+ 	return;
+  }
+
+  void clear_display(void) {
+ 	this -> display_idx = 0;
+ 	memset(this -> display_buf, 0, sizeof(this -> display_buf));
+ 	return;
+  }
+
+  void display(void) {
+ 	this -> set_offset(0, 0);
+ 	this -> write_pixel_buf(this -> display_buf, sizeof(this -> display_buf));
+ 	return;
+  }
+
+	void write_pixel(byte val) {
+		(this -> display_buf)[this -> display_idx++] = val;
+		this -> display_idx %= sizeof(this -> display_buf);
+		return;
+	}
+};
+
 #endif //_DISPLAY_H_
--- src.ino
+++ src.ino
@@ -8,69 +8,77 @@ LTR ltr_sensor;
 BME bme_sensor;
 SCD scd_sensor;
 Display display;
+OledDisplay oled_display;
 
 void setup() {
   Wire.begin();
   Serial.begin(9600);
   Serial.println(" ---- Setup ---- ");
-  display.init(3);
-  ltr_sensor.init();
-  ltr_sensor.set_gain(GAIN_2);
-  ltr_sensor.set_resolution(BIT_18);
-  ltr_sensor.info();
-  bme_sensor.init();
-  scd_sensor.init();
+  oled_display.init();
+  //display.init(3);
+//  ltr_sensor.init();
+//  ltr_sensor.set_gain(GAIN_2);
+//  ltr_sensor.set_resolution(BIT_18);
+//  ltr_sensor.info();
+//  bme_sensor.init();
+//  scd_sensor.init();
   Serial.println(" ---- End Setup ---- \n");
 }
 
-static uint32_t uvi = 0;
-static int32_t  t = 0;
-static uint32_t p = 0;
-static uint32_t h = 0;
-static scd_data_t scd_data = {0};
-static int i = 0;
+//static uint32_t uvi = 0;
+//static int32_t  t = 0;
+//static uint32_t p = 0;
+//static uint32_t h = 0;
+//static scd_data_t scd_data = {0};
+//static int i = 0;
 
 void loop() {
-	if ((i % 5) == 0) {
-		while (!(ltr_sensor.is_data_available())) {
-			delay(500);
-		}
+//	if ((i % 5) == 0) {
+//		while (!(ltr_sensor.is_data_available())) {
+//			delay(500);
+//		}
+//
+//		uvi = ltr_sensor.to_uvi(ltr_sensor.read_data_reg(UVS_DATA));
+//
+//		bme_sensor.sample();
+//		t = bme_sensor.get_temperature() / 100; // NOTE: rounding loses any decimal point
+//		p = (bme_sensor.get_pressure() >> 8) / 100;
+//		h = bme_sensor.get_humidity() >> 10;
+//
+//  		// NOTE: Should either use the low power mode periodic, or normal periodic measuring instead
+//  		scd_sensor.single_shot();
+//        scd_data = scd_sensor.read_measurement();
+//	}
+//
+// 	const int display_time = 2500;
+//	display.display(uvi);
+//	delay(display_time);
+// 	display.separator();
+// 	delay(1000);
+//
+//	display.display(t);
+//	delay(display_time);
+//	display.display(p);
+//	delay(display_time);
+//	display.display(h);
+//	delay(display_time);
+//    display.separator();
+//    delay(1000);
+//
+//    display.display(scd_data.c02);
+//	delay(display_time);
+//    display.display(scd_data.t);
+//	delay(display_time);
+//    display.display(scd_data.rh);
+//	delay(display_time);
+//	display.separator();
+//	delay(1000);
+//
 
-		uvi = ltr_sensor.to_uvi(ltr_sensor.read_data_reg(UVS_DATA));
+//  i++
 
-		bme_sensor.sample();
-		t = bme_sensor.get_temperature() / 100; // NOTE: rounding loses any decimal point
-		p = (bme_sensor.get_pressure() >> 8) / 100;
-		h = bme_sensor.get_humidity() >> 10;
-
-  		// NOTE: Should either use the low power mode periodic, or normal periodic measuring instead
-  		scd_sensor.single_shot();
-        scd_data = scd_sensor.read_measurement();
-	}
-
- 	const int display_time = 2500;
-	display.display(uvi);
-	delay(display_time);
- 	display.separator();
- 	delay(1000);
-
-	display.display(t);
-	delay(display_time);
-	display.display(p);
-	delay(display_time);
-	display.display(h);
-	delay(display_time);
-    display.separator();
-    delay(1000);
-
-    display.display(scd_data.c02);
-	delay(display_time);
-    display.display(scd_data.t);
-	delay(display_time);
-    display.display(scd_data.rh);
-	delay(display_time);
-	display.separator();
-	delay(1000);
-
-    i++;
+	oled_display.clear_display();
+	for (byte i = 0; i < 32; ++i) oled_display.print('A');
+ 	oled_display.display();
+ 	delay(2000);
 }
